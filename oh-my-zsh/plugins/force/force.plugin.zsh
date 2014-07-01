9141507b74d9984a0d22da7679069e5147b5231b
Adam Reese <areese@engineyard.com>
2014-07-01 15:27:32 -0700

    force helper queries

--- oh-my-zsh/plugins/force/force.plugin.zsh
+++ oh-my-zsh/plugins/force/force.plugin.zsh
@@ -14,19 +14,24 @@ fl() {
   local instance='na5.salesforce.com'
 
   case $1 in
-    "dev"|"development" )
-      username='areese@engineyard.com.developer'
-      instance='test'
-      ;;
-    "acc"|"acceptance" )
-      username='areese@engineyard.com.acceptance'
-      instance='test'
-      ;;
-    "partner" )
-      username='areese@engineyard.com.partner'
-      instance='test'
-      ;;
+	  "dev"|"development" )
+		  username='areese@engineyard.com.developer'
+		  instance='test'
+		  ;;
+	  "acc"|"acceptance" )
+		  username='areese@engineyard.com.acceptance'
+		  instance='test'
+		  ;;
+	  "partner" )
+		  username='areese@engineyard.com.partner'
+		  instance='test'
+		  ;;
   esac
 
   force login -u "${username}" -p "${password}" -i "${instance}" && echo "Logged in as $( force active )"
+}
+
+
+adoption() {
+	force query "SELECT COUNT(Id) total, ${1} FROM ${2} ORDER BY COUNT(Id) DESC"
 }
