13ea0f4d3f8b292706240b9fb98a755307ad47f1
Adam Reese <adam@reese.io>
2016-11-01 14:07:02 -0700

    ref(zsh): move zsh specific aliases to configs

---
+++ zsh/configs/aliases.zsh
@@ -0,0 +1,11 @@
+# vim: ft=zsh :
+
+# Split comma or colin separated data onto separate lines
+alias -g S="| tr ',:' '\n'"
+
+alias -g H='| head'
+alias -g T='| tail'
+alias -g G='| grep'
+alias -g L="| less"
+
+alias reload='exec zsh -l'
