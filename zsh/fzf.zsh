2f77c0895326631d814c77383b6bc6bdc26d7ce4
Adam Reese <adam@reese.io>
2017-03-18 20:02:58 -0700

    [zsh] make fzf not use entire window

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,18 +1,21 @@
 # zsh fzf
 # ------------------------------------------------------------------------------
 export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
-export FZF_DEFAULT_OPTS='
---color=fg:15,bg:-1,bg+:-1,pointer:1,info:7,hl+:4,hl:4
---bind=ctrl-u:page-up
---bind=ctrl-d:page-down
-'
 
-# Setup FZF
+_fzf_opts=(
+  --color=fg:15,bg:-1,bg+:-1,pointer:1,info:7,hl+:4,hl:4
+  --bind=ctrl-u:page-up
+  --bind=ctrl-d:page-down
+  --height 40%
+  --border
+)
+export FZF_DEFAULT_OPTS="${_fzf_opts}"
+
+unset _fzf_opts
+
+# Setup FZF paths
 # ------------------------------------------------------------------------------
 path+=(${HOME}/.fzf/bin(N-/))
-
-# Man path
-# ------------------------------------------------------------------------------
 manpath+=(${HOME}/.fzf/man(N-/))
 
 # Completion
