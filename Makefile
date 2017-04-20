5f8c10e86a792cfec740f63d265561fd6aad4700
Adam Reese <adam@reese.io>
2017-04-19 19:14:35 -0700

    [scripts] add new install script

--- Makefile
+++ Makefile
@@ -1,38 +1,7 @@
-DOTFILES := $(shell pwd)
-all: shell tmux _ruby _vim _git _postgres
+.PHONY: all
+all: symlink
+	./scripts/install.sh brew go python ruby
+
+.PHONY: shell
 shell:
-	ln -fs $(DOTFILES)/bashrc ${HOME}/.bashrc
-	ln -fs $(DOTFILES)/bashenv ${HOME}/.bashenv
-	ln -fs $(DOTFILES)/bashrc ${HOME}/.bashrc
-	ln -fs $(DOTFILES)/bash_profile ${HOME}/.bash_profile
-	ln -fs $(DOTFILES)/bash_prompt ${HOME}/.bash_prompt
-	ln -fs $(DOTFILES)/zshrc ${HOME}/.zshrc
-	ln -fs $(DOTFILES)/zshenv ${HOME}/.zshenv
-	ln -fs $(DOTFILES)/zlogin ${HOME}/.zlogin
-	ln -fs $(DOTFILES)/zlogout ${HOME}/.zlogout
-	ln -fs $(DOTFILES)/ackrc ${HOME}/.ackrc
-	ln -fs $(DOTFILES)/agignore ${HOME}/.agignore
-	ln -fs $(DOTFILES)/ctags ${HOME}/.ctags
-	ln -fs ${DOTFILES}/aliases ${HOME}/.aliases
-	ln -fs ${DOTFILES}/agignore ${HOME}/.agignore
-	# ln -fns ${DOTFILES}/zsh ${HOME}/.zsh
-tmux:
-	ln -fs $(DOTFILES)/tmux.conf ${HOME}/.tmux.conf
-_vim:
-	ln -fns $(DOTFILES)/vim ${HOME}/.vim
-	ln -fns $(DOTFILES)/vim ${HOME}/.config/nvim
-	ln -fs $(DOTFILES)/gvimrc ${HOME}/.gvimrc
-	curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-_ruby:
-	ln -fs $(DOTFILES)/irbrc ${HOME}/.irbrc
-	ln -fs $(DOTFILES)/pryrc ${HOME}/.pryrc
-	ln -fs ${DOTFILES}/gemrc ${HOME}/.gemrc
-	ln -fs ${DOTFILES}/rspec ${HOME}/.rspec
-_git:
-	ln -fns $(DOTFILES)/git ${HOME}/.config/git
-_postgres:
-	ln -fs $(DOTFILES)/psqlrc ${HOME}/.psqlrc
-_sqlite:
-	ln -fs $(DOTFILES)/sqliterc ${HOME}/.sqliterc
-_osx:
-	ln -fs $(DOTFILES)/slate ${HOME}/.slate
+	./scripts/install.sh symlink
---
+++ scripts/install.sh
@@ -0,0 +1,195 @@
+#!/usr/bin/env bash
+
+# Bash 'Strict Mode'
+# http://redsymbol.net/articles/unofficial-bash-strict-mode
+set -euo pipefail
+IFS=$'\n\t'
+
+DOTFILES=${DOTFILES:-${HOME}/.dotfiles}
+[[ -a "${DOTFILES}" ]] || die "${DOTFILES} directory does not exist"
+
+# die
+# -----------------------------------------------------------------------------
+die() {
+  # ${BASH_SOURCE[1]} is the file name of the caller.
+  local error="${1:-Unknown Error.}"
+  local code="${2:-1}"
+  printf >&2 "\e[0;31m  [✖] %s:%s: %s (exit %s)\e[0m\n" \
+    "${BASH_SOURCE[1]}" \
+    "${BASH_LINENO[0]}" \
+    "${error}" \
+    "${code}"
+  exit "${code}"
+}
+
+# onerror
+# -----------------------------------------------------------------------------
+onerror() {
+  local lasterr="$?"
+  local bash_command=${BASH_COMMAND}
+  die "${BASH_SOURCE[1]}:${BASH_LINENO[0]} ('$bash_command' exited with status $lasterr)"
+}
+trap onerror ERR
+
+# announce_step
+# -----------------------------------------------------------------------------
+announce_step() {
+  echo -e "\033[0;32m \u279E   \033[0;0m $*"
+}
+
+print_error() {
+  printf "\e[0;31m  [✖] %s %s\e[0m\n" "${1}" "${2}"
+}
+
+print_success() {
+  printf "\e[0;32m  [✔] %s\e[0m\n" "${1}"
+}
+
+# symlink
+# -----------------------------------------------------------------------------
+symlink() {
+  local sourcepath="${DOTFILES}/${1}"
+  local targetpath="${2}"
+  local fulltargetpath="${HOME}/${targetpath}"
+
+  # ensure the source exists
+  [[ -a "${sourcepath}" ]] || die "${sourcepath} does not exist"
+
+  if [[ -a "$fulltargetpath" ]]; then
+    local rp
+    rp=$(realpath "${fulltargetpath}")
+    if [[ "$rp" == "${sourcepath}" ]]; then
+      return
+    fi
+
+    echo "    ${targetpath} exists"
+    read -p "          Overwrite? [y/N] " -r
+    if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+      echo "    Skipped ${targetpath}"
+      return
+    fi
+  fi
+
+  echo -e "    Symlinking ${targetpath} \u279E ${sourcepath}"
+
+  mkdir -p "$(dirname "${fulltargetpath}")"
+  ln -fns "${sourcepath}" "${fulltargetpath}"
+}
+
+# install brew
+# -----------------------------------------------------------------------------
+install_homebrew() {
+  if ! command -v brew >/dev/null; then
+    announce_step "Installing Homebrew"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  fi
+}
+
+# symlink files
+# -----------------------------------------------------------------------------
+symlink_files() {
+  announce_step "Symlinking dotfiles"
+
+  # vim
+  symlink vim          .vim
+  symlink vim          .config/nvim
+  symlink vim/gvimrc   .gvimrc
+
+  # ruby
+	symlink ruby/irbrc   .irbrc
+	symlink ruby/pryrc   .pryrc
+	symlink ruby/gemrc   .gemrc
+	symlink ruby/rspec   .rspec
+
+  # git
+  symlink git          .config/git
+
+  # tmux
+  symlink tmux.conf    .tmux.conf
+
+  # screen
+  symlink screenrc     .screenrc
+
+  # database
+	symlink psqlrc       .psqlrc
+	symlink sqliterc     .sqliterc
+
+  # slate
+	symlink slate        .slate
+
+  # shell
+  symlink aliases      .aliases
+  symlink bash_profile .bash_profile
+  symlink bash_prompt  .bash_prompt
+  symlink bashrc       .bashrc
+  symlink zlogin       .zlogin
+  symlink zlogout      .zlogout
+  symlink zsh          .zsh
+  symlink zshrc        .zshrc
+
+  # search
+  symlink ackrc        .ackrc
+  symlink agignore     .agignore
+
+  # ctags
+  symlink ctags        .ctags
+
+  # bookmarks
+  symlink bookmarks    .bookmarks
+
+  # bin
+  symlink bin          .local/bin
+
+  print_success "Completed symlinks"
+}
+
+install_python_packages() {
+  echo "Updating global pip"
+  pip3 install --upgrade pip
+
+  echo "Updating global pip requirements"
+  pip3 install --upgrade --requirement "${DOTFILES}/python/requirements.txt"
+}
+
+install_ruby_gems() {
+  echo "Installing default gems"
+  while read -r gemname; do
+    gem install "$gemname"
+  done < "${DOTFILES}/ruby/default-gems"
+}
+
+install_go_packages() {
+  echo "Installing default go packages"
+  while read -r pkg; do
+    echo "${pkg}"
+    go get -u --fix "${pkg}"
+  done < "${DOTFILES}/go/default-packages"
+}
+
+usage() {
+  echo "${0} {brew, go, python, ruby, symlink}"
+}
+
+# -----------------------------------------------------------------------------
+# main
+# -----------------------------------------------------------------------------
+
+# TODO terminfo
+# TODO brew
+# TODO macos defaults
+
+main() {
+  while (( "$#" > 0 )); do
+    case "$1" in
+      brew)    install_homebrew        ;;
+      go)      install_go_packages     ;;
+      python)  install_python_packages ;;
+      ruby)    install_ruby_gems       ;;
+      symlink) symlink_files           ;;
+      *)       usage                   ;;
+    esac
+    shift
+  done
+}
+
+main "$@"
