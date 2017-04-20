322d20f6c5c303886073674583eb8bbadb9363db
Adam Reese <adam@reese.io>
2017-04-19 18:59:07 -0700

    [zsh] add global alias for panicparse

--- zsh/go.zsh
+++ zsh/go.zsh
@@ -4,14 +4,28 @@ if (( ! $+commands[go] )); then
   return 1
 fi
 
+# Setup PATH for go
+# -----------------------------------------------------------------------------
 export GOPATH=${HOME}/p/go
-
 path=(${GOPATH}/bin $path)
 
+# -----------------------------------------------------------------------------
+# gocover
+# Run test coverage on a package and open the results in a browser.
 gocover() {
   go test ${1} -coverprofile=$TMPDIR/coverage.out
   go tool cover -html=$TMPDIR/coverage.out
 }
+
+# -----------------------------------------------------------------------------
+# Global alias pipe to panicparse.
+#
+# Usage:
+#
+#    go test PP
+#
+# Depends on github.com/maruel/panicparse
+alias -g PP='|& pp'
 
 # -----------------------------------------------------------------------------
 # vim: ft=zsh :
