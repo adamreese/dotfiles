0ce27414950ae2db57ec2387fe1586ff79530d55
Adam Reese <adam@reese.io>
2017-05-12 17:08:41 -0700

    [zsh] use built in completion for helm

--- zsh/kubernetes.zsh
+++ zsh/kubernetes.zsh
@@ -4,29 +4,43 @@ if (( ! $+commands[kubectl] )); then
   return 1
 fi
 
-# Aliases
-# -----------------------------------------------------------------------------
-alias k=kubectl
-
 # Completion
 # -----------------------------------------------------------------------------
 autoload -Uz zrecompile
-typeset kubecache=${HOME}/.cache/zsh/kubectl.zsh
 
-if [[ ! -s $kubecache || ${commands[kubectl]} -nt $kubecache ]]; then
-  kubectl completion zsh | egrep -v 'compinit' >| $kubecache
-fi
-zrecompile -p "${kubecache}" && command rm -f "${kubecache}.zwc.old"
-[[ -s "${kubecache}.zwc" ]] || zcompile $kubecache
-source $kubecache
+# zcmp
+#
+# compile and source cobra style zsh completions
+zcmp() {
+  typeset cachefile=${HOME}/.cache/zsh/${1}.zsh
 
-unset kubecache
+  if [[ ! -s $cachefile || ${commands[${1}]} -nt $cachefile ]]; then
+    "${1}" completion zsh | egrep -v 'compinit' >| $cachefile
+  fi
+
+  zrecompile -p "${cachefile}" && command rm -f "${cachefile}.zwc.old"
+  [[ -s "${cachefile}.zwc" ]] || zcompile $cachefile
+  source $cachefile
+}
+
+
+# Kubernetes
+# -----------------------------------------------------------------------------
+alias k=kubectl
+
+zcmp kubectl
 
 # Helm
 # -----------------------------------------------------------------------------
 export HELM_HOST=:44134
 
 path=(${GOPATH}/src/k8s.io/helm/bin ${path})
+
+# HL global alias
+# example: `helm get HL`
+alias -g HL='$(helm last)'
+
+zcmp helm
 
 # -----------------------------------------------------------------------------
 # vim: ft=zsh :
