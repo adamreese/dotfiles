11d38c416ef9da6d2f751864976bf7e3fcc337a1
Adam Reese <adam@reese.io>
2017-10-14 20:50:28 -0700

    [zsh] update docker functions, move to xdg

--- zsh/docker.zsh
+++ zsh/docker.zsh
@@ -9,12 +9,23 @@
 docker-clean() {
   emulate -L zsh
 
-  echo "Deleting stale containers..."
-  command docker rm $(docker ps --filter status=exited -q 2>/dev/null) 2>/dev/null
-  echo "Deleting stale images..."
-  command docker rmi $(docker images --filter dangling=true -q 2>/dev/null) 2>/dev/null
-  echo "Deleting stale volumes..."
-  command docker volume rm $(docker volume ls --filter dangling=true -q 2>/dev/null) 2>/dev/null
+  typeset -a _containers _images _volumes
+  _containers=($(docker ps --filter status=exited -q 2>/dev/null))
+  _images=($(docker images --filter dangling=true -q 2>/dev/null))
+  _volumes=($(docker volume ls --filter dangling=true -q 2>/dev/null))
+
+  if (( $#_containers > 0 )); then
+    echo "Deleting stale containers..."
+    command docker rm "${_containers[@]}"
+  fi
+  if (( $#_images > 0 )); then
+    echo "Deleting stale images..."
+    command docker rmi "${_images[@]}"
+  fi
+  if (( $#_volumes > 0 )); then
+    echo "Deleting stale volumes..."
+    command docker volume rm "${_volumes[@]}"
+  fi
 }
 
 docker-nuke() {
@@ -33,15 +44,17 @@ docker-nuke() {
   fi
 }
 
-docker-compact-disk() {
-  cd ~/Library/Containers/com.docker.docker/Data/com.docker.driver.amd64-linux || return
-  /Applications/Docker.app/Contents/MacOS/qemu-img convert -p -O qcow2 Docker.qcow2 Docker2.qcow2
-  mv Docker2.qcow2 Docker.qcow2
-}
+if [[ $(uname -s) == "Darwin" ]]; then
+  docker-compact-disk() {
+    cd ~/Library/Containers/com.docker.docker/Data/com.docker.driver.amd64-linux || return
+    /Applications/Docker.app/Contents/MacOS/qemu-img convert -p -O qcow2 Docker.qcow2 Docker2.qcow2
+    mv Docker2.qcow2 Docker.qcow2
+  }
 
-docker-screen() {
-  screen -D -R -S moby ${HOME}/Library/Containers/com.docker.docker/Data/com.docker.driver.amd64-linux/tty
-}
+  docker-screen() {
+    screen -D -R -S moby ${HOME}/Library/Containers/com.docker.docker/Data/com.docker.driver.amd64-linux/tty
+  }
+fi
 
 # -----------------------------------------------------------------------------
 # vim:ft=zsh
