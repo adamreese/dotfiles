78115a24e137b645f32ab7e18bc09b1b725c11f9
Adam Reese <adam@reese.io>
2017-10-16 09:44:38 -0700

    [zsh] move macos aliases to file

---
+++ zsh/aliases.macos.zsh
@@ -0,0 +1,27 @@
+# zsh aliases
+# -----------------------------------------------------------------------------
+
+# masOS specific command aliases
+[[ "${OSTYPE}" == "Darwin" ]] && return
+
+# bundle
+alias   be='bundle-exec-hack'
+alias   bi='bundle-hack install'
+alias   br='bundle-exec-hack rspec'
+alias  beg='bundle-exec-hack guard'
+alias  bil='bundle install --local'
+alias brff='bundle-exec-hack rspec --fail-fast'
+
+# Show/hide hidden files in Finder
+alias show='defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder'
+alias hide='defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder'
+
+# Update and cleanup brew
+alias  bubu='brew update && brew upgrade && brew cleanup'
+alias bubua='brew update && brew upgrade --fetch-HEAD && brew cleanup'
+
+# Flush dns cache
+alias flush='sudo killall -HUP mDNSResponder'
+
+# -----------------------------------------------------------------------------
+# vim:ft=zsh
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -50,29 +50,6 @@ if [[ $TERM == *"tmux"* ]]; then
   alias :vs='tmux split-window -h'
 fi
 
-# masOS specific command aliases
-if [[ $(uname -s) = "Darwin" ]]; then
-
-  # bundle
-  alias   be='bundle-exec-hack'
-  alias   bi='bundle-hack install'
-  alias   br='bundle-exec-hack rspec'
-  alias  beg='bundle-exec-hack guard'
-  alias  bil='bundle install --local'
-  alias brff='bundle-exec-hack rspec --fail-fast'
-
-  # Show/hide hidden files in Finder
-  alias show='defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder'
-  alias hide='defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder'
-
-  # Update and cleanup brew
-  alias  bubu='brew update && brew upgrade && brew cleanup'
-  alias bubua='brew update && brew upgrade --fetch-HEAD && brew cleanup'
-
-  # Flush dns cache
-  alias flush='sudo killall -HUP mDNSResponder'
-fi
-
 if (( ${+commands[yaourt]} )); then
   alias archup='sudo pacman -Syu && yaourt -Syua --noconfirm'
 fi
