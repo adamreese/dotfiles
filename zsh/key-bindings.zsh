d2c069277b16ddcad83d7b14619ca02c3a5ac2ac
Adam Reese <adam@reese.io>
2017-12-12 11:34:27 -0800

    [zsh] cleanup zsh bindings

--- zsh/key-bindings.zsh
+++ zsh/key-bindings.zsh
@@ -15,79 +15,57 @@ if (( ${+terminfo[smkx]} )) && (( ${+terminfo[rmkx]} )); then
   zle -N zle-line-finish
 fi
 
+typeset -A key
+key[Delete]=$terminfo[kdch1]
+key[Up]=$terminfo[kcuu1]
+key[Down]=$terminfo[kcud1]
+key[Left]=$terminfo[kcub1]
+key[Right]=$terminfo[kcuf1]
+key[ShiftTab]=$terminfo[kcbt]
+
 bindkey -e                                            # Use emacs key bindings
 
 bindkey '\ew' kill-region                             # [Esc-w] - Kill from the cursor to the mark
-bindkey -s '\el' 'ls\n'                               # [Esc-l] - run command: ls
-bindkey '^r' history-incremental-search-backward      # [Ctrl-r] - Search backward incrementally for a specified string. The string may begin with ^ to anchor the search to the beginning of the line.
-if [[ "${terminfo[kpp]}" != "" ]]; then
-  bindkey "${terminfo[kpp]}" up-line-or-history       # [PageUp] - Up a line of history
-fi
-if [[ "${terminfo[knp]}" != "" ]]; then
-  bindkey "${terminfo[knp]}" down-line-or-history     # [PageDown] - Down a line of history
-fi
+bindkey '^r'  history-incremental-search-backward     # [Ctrl-r] - Search backward incrementally for a specified string. The string may begin with ^ to anchor the search to the beginning of the line.
 
 # start typing + [Up-Arrow] - fuzzy find history forward
-if [[ "${terminfo[kcuu1]}" != "" ]]; then
-  autoload -U up-line-or-beginning-search
-  zle -N up-line-or-beginning-search
-  bindkey "${terminfo[kcuu1]}" up-line-or-beginning-search
+if [[ -n $key[Up] ]]; then
+  autoload -U             up-line-or-beginning-search
+  zle      -N             up-line-or-beginning-search
+  bindkey  "${key[Up]}"   up-line-or-beginning-search
 fi
 # start typing + [Down-Arrow] - fuzzy find history backward
-if [[ "${terminfo[kcud1]}" != "" ]]; then
-  autoload -U down-line-or-beginning-search
-  zle -N down-line-or-beginning-search
-  bindkey "${terminfo[kcud1]}" down-line-or-beginning-search
+if [[ -n "${key[Down]}" ]]; then
+  autoload -U             down-line-or-beginning-search
+  zle      -N             down-line-or-beginning-search
+  bindkey  "${key[Down]}" down-line-or-beginning-search
 fi
 
-if [[ "${terminfo[khome]}" != "" ]]; then
-  bindkey "${terminfo[khome]}" beginning-of-line      # [Home] - Go to beginning of line
-fi
-if [[ "${terminfo[kend]}" != "" ]]; then
-  bindkey "${terminfo[kend]}"  end-of-line            # [End] - Go to end of line
-fi
-
-bindkey ' ' magic-space                               # [Space] - do history expansion
+bindkey ' '       magic-space                         # [Space] - do history expansion
 
 bindkey '^[[1;5C' forward-word                        # [Ctrl-RightArrow] - move forward one word
 bindkey '^[[1;5D' backward-word                       # [Ctrl-LeftArrow] - move backward one word
 
-if [[ "${terminfo[kcbt]}" != "" ]]; then
-  bindkey "${terminfo[kcbt]}" reverse-menu-complete   # [Shift-Tab] - move through the completion menu backwards
+if [[ -n "${key[ShiftTab]}" ]]; then
+  bindkey "${key[ShiftTab]}" reverse-menu-complete    # [Shift-Tab] - move through the completion menu backwards
 fi
 
 bindkey '^?' backward-delete-char                     # [Backspace] - delete backward
-if [[ "${terminfo[kdch1]}" != "" ]]; then
-  bindkey "${terminfo[kdch1]}" delete-char            # [Delete] - delete forward
+if [[ -n "${key[Delete]}" ]]; then
+  bindkey "${key[Delete]}" delete-char                # [Delete] - delete forward
 else
-  bindkey "^[[3~" delete-char
+  bindkey "^[[3~"  delete-char
   bindkey "^[3;5~" delete-char
-  bindkey "\e[3~" delete-char
+  bindkey "\e[3~"  delete-char
 fi
 
 # Edit the current command line in $EDITOR
-autoload -U edit-command-line
-zle -N edit-command-line
-bindkey '\C-x\C-e' edit-command-line
+autoload -U         edit-command-line
+zle      -N         edit-command-line
+bindkey  '\C-x\C-e' edit-command-line
 
 # file rename magick
-bindkey "^[m" copy-prev-shell-word
+bindkey  "^[m"      copy-prev-shell-word
 
-# consider emacs keybindings:
-
-#bindkey -e  ## emacs key bindings
-#
-#bindkey '^[[A' up-line-or-search
-#bindkey '^[[B' down-line-or-search
-#bindkey '^[^[[C' emacs-forward-word
-#bindkey '^[^[[D' emacs-backward-word
-#
-#bindkey -s '^X^Z' '%-^M'
-#bindkey '^[e' expand-cmd-path
-#bindkey '^[^I' reverse-menu-complete
-#bindkey '^X^N' accept-and-infer-next-history
-#bindkey '^W' kill-region
-#bindkey '^I' complete-word
-## Fix weird sequence that rxvt produces
-#bindkey -s '^[[Z' '\t'
-#
+# -----------------------------------------------------------------------------
+# vim:ft=zsh
