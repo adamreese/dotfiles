b62b4523d2ba42963890695621b04aed933eba00
Adam Reese <adam@reese.io>
2018-01-19 21:31:13 -0800

    [zsh] portable gpg socket

--- zsh/gpg.zsh
+++ zsh/gpg.zsh
@@ -7,12 +7,16 @@ if (( ! $+commands[gpg-agent] )); then
   return 1
 fi
 
+gpg-refresh-agent() {
+  gpg-connect-agent updatestartuptty /bye >/dev/null
+}
+
 gpgconf --launch gpg-agent
 
 # Set SSH to use gpg-agent if it is configured to do so
 if grep -q '^enable-ssh-support' "${GNUPGHOME}/gpg-agent.conf" /dev/null 2>&1; then
   unset SSH_AGENT_PID
-  export SSH_AUTH_SOCK="${GNUPGHOME}/S.gpg-agent.ssh"
+  export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
 fi
 
 # -----------------------------------------------------------------------------
