c440abdc22d5e83f566d8d203d86b012a0d01997
Adam Reese <adam@reese.io>
2018-03-02 08:49:57 -0800

    scripts: fix node/ruby installers

--- node/install.sh
+++ node/install.sh
@@ -4,7 +4,10 @@ set -euo pipefail
 DOTFILES=${DOTFILES:-${HOME}/.dotfiles}
 [[ -a "${DOTFILES}" ]] || die "${DOTFILES} directory does not exist"
 
-echo "Installing default gems"
-while read -r gemname; do
-  gem install "$gemname"
-done < "${DOTFILES}/ruby/default-gems"
+echo "Installing default node packages"
+
+npm install --global npm
+
+while read -r pkg; do
+  npm install --global --production "${pkg}"
+done < "${DOTFILES}/node/default-packages"
--- ruby/install.sh
+++ ruby/install.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 DOTFILES=${DOTFILES:-${HOME}/.dotfiles}
 [[ -a "${DOTFILES}" ]] || die "${DOTFILES} directory does not exist"
 
-echo "Installing default node packages"
-while read -r pkg; do
-  npm install --global --production "${pkg}"
-done < "${DOTFILES}/node/default-packages"
+echo "Installing default gems"
+while read -r gemname; do
+  gem install "$gemname"
+done < "${DOTFILES}/ruby/default-gems"
