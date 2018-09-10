6cc217ca32ada4d4a38bc46370265bece21fe1e4
Adam Reese <adam@reese.io>
2018-09-10 14:03:54 -0700

    [scripts] fix minikube install script

--- scripts/macos/minikube.sh
+++ scripts/macos/minikube.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -euxo pipefail
 
 # Download an OSX binary minikube
 # https://github.com/kubernetes/minikube/releases
-# Put it somewhere that you can run it from: /usr/local/bin/minikube && chmod +x /usr/local/bin/minikube
 
 # minikube-darwin-amd64
 
@@ -11,9 +10,9 @@ echo
 echo "ᴍɪɴɪᴋᴜʙᴇ :: installing latest minikube"
 echo
 
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 &&
-  chmod +x minikube &&
-  sudo mv minikube /usr/local/bin/
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-darwin-amd64
+chmod +x minikube
+sudo mv minikube /usr/local/bin/
 
 minikube version
 
@@ -24,8 +23,8 @@ echo
 echo "ᴍɪɴɪᴋᴜʙᴇ :: installing hyperkit driver"
 echo
 
-curl -LO https://storage.googleapis.com/minikube/releases/latest/docker-machine-driver-hyperkit &&
-  chmod +x docker-machine-driver-hyperkit &&
-  sudo mv docker-machine-driver-hyperkit /usr/local/bin/ &&
-  sudo chown root:wheel /usr/local/bin/docker-machine-driver-hyperkit &&
-  sudo chmod u+s /usr/local/bin/docker-machine-driver-hyperkit
+curl -LO https://storage.googleapis.com/minikube/releases/latest/docker-machine-driver-hyperkit
+chmod +x docker-machine-driver-hyperkit
+sudo mv docker-machine-driver-hyperkit /usr/local/bin/
+sudo chown root:wheel /usr/local/bin/docker-machine-driver-hyperkit
+sudo chmod u+s /usr/local/bin/docker-machine-driver-hyperkit
