96baab7e4894a2051dbbc6a6879de73189a7fed9
Adam Reese <adam@reese.io>
2019-01-05 11:33:34 -0800

    [zsh] add rustup completion

--- zsh/rust.zsh
+++ zsh/rust.zsh
@@ -5,5 +5,15 @@
 # -----------------------------------------------------------------------------
 path+=${HOME}/.cargo/bin
 
+(( ${+commands[rustup]} )) || return
+
+() {
+  local cache=${ZDOTDIR:?}/completion/_rustup
+
+  if [[ ! -s $cache || ${commands[rustup]} -nt $cache ]]; then
+    rustup completions zsh >| $cache
+  fi
+} || echo 'Failed to write rustup completion' >&2
+
 # -----------------------------------------------------------------------------
 # vim: ft=zsh :
