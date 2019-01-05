25cbfd981bbc0949b5ef6cc40507b22858adb4ec
Adam Reese <adam@reese.io>
2019-01-05 09:53:00 -0800

    [zsh] chruby takes forever to load
    Need to find a better solution

--- zsh/chruby.zsh
+++ zsh/chruby.zsh
@@ -1,5 +1,8 @@
 # zsh chruby
 # -----------------------------------------------------------------------------
+path[1,0]=/usr/local/opt/ruby/bin
+return
+
 (( ${+commands[chruby-exec]} )) || return
 
 source "${commands[chruby-exec]:h:h}/share/chruby/chruby.sh"
