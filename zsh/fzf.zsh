40562266c66c3aa85c3ef84a37fec2c910930d43
Adam Reese <adam@reese.io>
2020-02-11 14:17:30 -0800

    [zsh] remove unused fzf functions

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -7,41 +7,32 @@ if (( ${+commands[fd]} )); then
 else
   typeset -gx FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
 fi
-typeset -gx FZF_DEFAULT_OPTS='--color=fg:15,bg:-1,bg+:-1,pointer:1,info:7,hl+:4,hl:4 --multi --bind=ctrl-u:page-up,ctrl-d:page-down'
-typeset -gx FZF_CTRL_T_OPTS='--select-1 --exit-0 --preview "(highlight -O ansi -l {} || cat {} || tree -C {}) 2> /dev/null | head -200" --bind "?:toggle-preview"'
 
-export FZF_COMPLETION_TRIGGER='``'
-
-# CTRL-T - Paste the selected file path(s) into the command line
-fzf-sel() {
-  setopt localoptions pipefail 2> /dev/null
-  local opts="--height 40% --reverse ${FZF_DEFAULT_OPTS} ${FZF_CTRL_T_OPTS}"
-  while read item; do
-    echo -n "${(q)item} "
-  done < <(eval "${FZF_DEFAULT_COMMAND} | fzf ${opts} $@")
-  local ret=$?
-  echo
-  return $ret
+function {
+  local _opts=(
+    --color 'fg:15,bg:-1,bg+:-1,pointer:1,info:7,hl+:4,hl:4'
+    --multi
+    --bind 'ctrl-u:page-up,ctrl-d:page-down'
+    --bind '?:toggle-preview'
+    --preview-window 'right:62%:hidden'
+  )
+  typeset -gx FZF_DEFAULT_OPTS="${(f)_opts}"
 }
 
-widget-fzf-file() {
-  LBUFFER="${LBUFFER}$(fzf-sel)"
-  local ret=$?
-  zle redisplay
-  typeset -f zle-line-init >/dev/null && zle zle-line-init
-  return $ret
-}
-zle     -N   widget-fzf-file
-bindkey '^T' widget-fzf-file
+# ------------------------------------------------------------------------------
 
-widget-fzf-branch() {
-  if git rev-parse --git-dir &>/dev/null; then
-    git-fco
-    zle accept-line
-  fi
+# Use fd (https://github.com/sharkdp/fd) instead of the default find
+# command for listing path candidates.
+# - The first argument to the function ($1) is the base path to start traversal
+# - See the source code (completion.{bash,zsh}) for the details.
+_fzf_compgen_path() {
+  fd --hidden --follow --exclude ".git" . "$1"
 }
-zle     -N   widget-fzf-branch
-bindkey '^B' widget-fzf-branch
+
+# Use fd to generate the list for directory completion
+_fzf_compgen_dir() {
+  fd --type d --hidden --follow --exclude ".git" . "$1"
+}
 
 if [[ -s "/usr/local/opt/fzf/shell/completion.zsh" ]]; then
   source "/usr/local/opt/fzf/shell/completion.zsh"
