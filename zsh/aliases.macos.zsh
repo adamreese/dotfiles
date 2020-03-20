19e4175d58ddb1e9feb6eb4e4e49b562ca4c576a
Adam Reese <adam@reese.io>
2020-03-20 14:06:24 -0700

    [zsh] remove unused aliases

--- zsh/aliases.macos.zsh
+++ zsh/aliases.macos.zsh
@@ -4,10 +4,6 @@
 # masOS specific command aliases
 [[ "${OSTYPE}" == darwin* ]] || return
 
-# Show/hide hidden files in Finder
-alias show='defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder'
-alias hide='defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder'
-
 # Update and cleanup brew
 alias     bubu='brew update && brew upgrade && brew cleanup -s'
 alias    bubua='brew update && brew upgrade --fetch-HEAD && brew cleanup -s'
