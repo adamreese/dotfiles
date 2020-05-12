e9f3452fb18c072dead47aa7aa4fbdebe759fa24
Adam Reese <adam@reese.io>
2020-05-12 11:22:23 -0700

    [zsh] remove travis for load time

--- zsh/travis.zsh
+++
@@ -1,6 +0,0 @@
-# zsh travis
-# -----------------------------------------------------------------------------
-
-if [[ -s "${HOME}/.travis/travis.sh" ]]; then
-  source "${HOME}/.travis/travis.sh"
-fi
