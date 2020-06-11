c02f4b39ad36ad353adc3d3bdffedfe54f721e65
Adam Reese <adam@reese.io>
2020-06-11 12:03:38 -0700

    [zsh] support gdircolors

--- zsh/appearance.zsh
+++ zsh/appearance.zsh
@@ -1,15 +1,14 @@
 # ls colors
+# ------------------------------------------------------------------------------
 
-# # Enable ls colors
-# export LSCOLORS='exfxcxdxbxegedabagacad'
-# export LS_COLORS='rs=0:di=01;34:ln=01;36:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:mi=00:su=37;41:sg=30;43:ca=30;41:tw=30;42:ow=34;42:st=37;44:ex=01;32:'
-
-if (( $+commands[dircolors] )); then
-  if (( ! ${+LS_COLORS} )) && [[ -s ${XDG_CONFIG_HOME}/dircolors ]]; then
+if (( ! ${+LS_COLORS} )) && [[ -s ${XDG_CONFIG_HOME}/dircolors ]]; then
+  if (( $+commands[dircolors] )); then
     eval "$(dircolors --sh ${XDG_CONFIG_HOME}/dircolors)"
-  else
-    export LS_COLORS='di=1;34:ln=35:so=32:pi=33:ex=31:bd=1;36:cd=1;33:su=30;41:sg=30;46:tw=30;42:ow=30;43'
+  elif (( $+commands[gdircolors] )); then
+    eval "$(gdircolors --sh ${XDG_CONFIG_HOME}/dircolors)"
   fi
+else
+  export LS_COLORS='di=1;34:ln=35:so=32:pi=33:ex=31:bd=1;36:cd=1;33:su=30;41:sg=30;46:tw=30;42:ow=30;43'
 fi
 
 (( $+commands[gls] )) || alias gls='ls'
@@ -22,5 +21,3 @@ alias l='ls -l'
 # Take advantage of $LS_COLORS for completion as well.
 zstyle ':completion:*'         list-colors "${(s.:.)LS_COLORS}"
 zstyle ':completion:*:default' list-colors "${(s.:.)LS_COLORS}"
-
-[[ -n "$WINDOW" ]] && SCREEN_NO="%B$WINDOW%b " || SCREEN_NO=""
