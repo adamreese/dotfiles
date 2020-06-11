2b26b1aa9780b474679aed42b9815c78dc027449
Adam Reese <adam@reese.io>
2020-06-11 09:08:55 -0700

    [hammerspoon] new window movement key bindings

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -20,13 +20,8 @@ hs.console.toolbar(nil)
 --------------------------------------------------------------------------------
 -- Key Bindings
 --------------------------------------------------------------------------------
---
--- Note: macOS custom bindings
---     hyper + D   Toggle Do Not Disturb
---     hyper + N   Toggle Notification Center
---
 
-local hyper = {'cmd', 'alt', 'ctrl'}
+local hyper = {'cmd', 'alt', 'ctrl', 'shift'}
 
 hs.hotkey.bind(hyper, 'c', hs.toggleConsole)
 
@@ -57,28 +52,19 @@ Install:andUse('KSheet', {
 -- http://www.hammerspoon.org/Spoons/WindowHalfsAndThirds.html
 Install:andUse("WindowHalfsAndThirds", {
     hotkeys = {
-      left_half    = { {"ctrl", "cmd"}, "Left" },
-      right_half   = { {"ctrl", "cmd"}, "Right" },
-      top_half     = { {"ctrl", "cmd"}, "Up" },
-      bottom_half  = { {"ctrl", "cmd"}, "Down" },
-
       top_left     = { hyper, "1" },
       top_right    = { hyper, "2" },
       bottom_left  = { hyper, "3" },
       bottom_right = { hyper, "4" },
-      max_toggle   = { hyper, "f" },
       max          = { hyper, "5" },
+      max_toggle   = { hyper, "f" },
 
-      third_left   = { {"ctrl", "alt"}, "Left" },
-      third_right  = { {"ctrl", "alt"}, "Right" },
-      third_up     = { {"ctrl", "alt"}, "Up" },
-      third_down   = { {"ctrl", "alt"}, "Down" },
-
-
-      undo         = { {"alt", "cmd"}, "z" },
-      center       = { {"alt", "cmd"}, "c" },
-      larger       = { {"alt", "cmd", "shift"}, "Right" },
-      smaller      = { {"alt", "cmd", "shift"}, "Left" },
+      left_half    = { {"ctrl", "cmd"}, "Left" },
+      right_half   = { {"ctrl", "cmd"}, "Right" },
+      top_half     = { {"ctrl", "cmd"}, "Up" },
+      bottom_half  = { {"ctrl", "cmd"}, "Down" },
+      undo         = { {"ctrl", "cmd"}, "z" },
+      center       = { {"ctrl", "cmd"}, "c" },
     }
   })
 
