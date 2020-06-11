ad79df81ba90049f1e3d9707d13930afbb86b762
Adam Reese <adam@reese.io>
2020-06-11 11:47:39 -0700

    [zsh] simplify key bindings

--- zsh/key-bindings.zsh
+++ zsh/key-bindings.zsh
@@ -3,43 +3,8 @@
 # http://zsh.sourceforge.net/Doc/Release/Zsh-Line-Editor.html#Standard-Widgets
 
 if [[ ${TERM} == 'dumb' ]]; then
-  return 1
+  return
 fi
-
-zmodload -F zsh/terminfo +b:echoti +p:terminfo
-typeset -gA key_info
-key_info=(
-  'Control'      '\C-'
-  'ControlLeft'  '\e[1;5D \e[5D \e\e[D \eOd \eOD'
-  'ControlRight' '\e[1;5C \e[5C \e\e[C \eOc \eOC'
-  'Escape'       '\e'
-  'Meta'         '\M-'
-  'Space'        ' '
-  'Backspace'    ${terminfo[kbs]}
-  'ShiftTab'     ${terminfo[kcbt]}
-  'Left'         ${terminfo[kcub1]}
-  'Down'         ${terminfo[kcud1]}
-  'Right'        ${terminfo[kcuf1]}
-  'Up'           ${terminfo[kcuu1]}
-  'Delete'       ${terminfo[kdch1]}
-  'End'          ${terminfo[kend]}
-  'F1'           ${terminfo[kf1]}
-  'F2'           ${terminfo[kf2]}
-  'F3'           ${terminfo[kf3]}
-  'F4'           ${terminfo[kf4]}
-  'F5'           ${terminfo[kf5]}
-  'F6'           ${terminfo[kf6]}
-  'F7'           ${terminfo[kf7]}
-  'F8'           ${terminfo[kf8]}
-  'F9'           ${terminfo[kf9]}
-  'F10'          ${terminfo[kf10]}
-  'F11'          ${terminfo[kf11]}
-  'F12'          ${terminfo[kf12]}
-  'Home'         ${terminfo[khome]}
-  'Insert'       ${terminfo[kich1]}
-  'PageDown'     ${terminfo[knp]}
-  'PageUp'       ${terminfo[kpp]}
-)
 
 # Use emacs key bindings
 bindkey -e
@@ -48,53 +13,26 @@ bindkey -e
 bindkey '\ew' kill-region
 
 # [Ctrl-r] - Search backward incrementally for a specified string. The string may begin with ^ to anchor the search to the beginning of the line.
-bindkey "$key_info[Control]R" history-incremental-search-backward
-
-# start typing + [Up-Arrow] - fuzzy find history forward
-if [[ -n $key_info[Up] ]]; then
-  autoload -U                up-line-or-beginning-search
-  zle      -N                up-line-or-beginning-search
-  bindkey  "${key_info[Up]}" up-line-or-beginning-search
-fi
-# start typing + [Down-Arrow] - fuzzy find history backward
-if [[ -n "${key_info[Down]}" ]]; then
-  autoload -U                  down-line-or-beginning-search
-  zle      -N                  down-line-or-beginning-search
-  bindkey  "${key_info[Down]}" down-line-or-beginning-search
-fi
+bindkey '^[R' history-incremental-search-backward
 
 # [Space] - do history expansion
-# bindkey ' '       magic-space
-
-# [Ctrl-RightArrow] - move forward one word
-bindkey '^[[1;5C' forward-word
-# [Ctrl-LeftArrow] - move backward one word
-bindkey '^[[1;5D' backward-word
-
-# [Shift-Tab] - move through the completion menu backwards
-if [[ -n "${key_info[ShiftTab]}" ]]; then
-  bindkey "${key_info[ShiftTab]}" reverse-menu-complete
-fi
+bindkey ' ' magic-space
 
 # [Backspace] - delete backward
 bindkey '^?' backward-delete-char
 
-# [Delete] - delete forward
-if [[ -n "${key_info[Delete]}" ]]; then
-  bindkey "${key_info[Delete]}" delete-char
-else
-  bindkey "^[[3~"  delete-char
-  bindkey "^[3;5~" delete-char
-  bindkey "\e[3~"  delete-char
-fi
-
 # Edit the current command line in $EDITOR
-autoload -U         edit-command-line
-zle      -N         edit-command-line
-bindkey  "$key_info[Control]X$key_info[Control]E" edit-command-line
+autoload -U     edit-command-line
+zle      -N     edit-command-line
+bindkey  '^X^E' edit-command-line
 
-# file rename magick
-bindkey  '^[m'      copy-prev-shell-word
+zmodload -i zsh/complist
+
+# [Shift-Tab] - move through the completion menu backwards
+bindkey -M menuselect '^[[Z' reverse-menu-complete
+
+# [Ctrl-C] - exit menu
+bindkey -M menuselect '^C' reset-prompt
 
 export KEYTIMEOUT=1
 
@@ -121,4 +59,4 @@ expand-aliases() {
 zle -N expand-aliases
 
 # [Control-x-Space] - do global alias expansion
-bindkey "$key_info[Control]X$key_info[Space]" expand-aliases
+bindkey '^X^ ' expand-aliases
