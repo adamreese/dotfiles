498729a46eba7d41ccd9f0eef5393a34319884b4
Adam Reese <adam@reese.io>
2020-06-11 11:52:48 -0700

    [zsh] remove glcoud config

--- zsh/gcloud.zsh
+++
@@ -1,8 +0,0 @@
-# zsh gcloud
-# -----------------------------------------------------------------------------
-
-: ${GCLOUD_SDK_ROOT:=/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk}
-
-[[ -e $GCLOUD_SDK_ROOT ]] || return
-
-path[1,0]=${GCLOUD_SDK_ROOT}/bin
