75374877b68b303333a8ff21fc974ddad55d3219
Adam Reese <adam@reese.io>
2020-06-30 12:42:09 -0700

    [zsh] format zsh options

--- zsh/environment.zsh
+++ zsh/environment.zsh
@@ -1,34 +1,33 @@
 # zsh environment
 # -----------------------------------------------------------------------------
 
-# General
-# -----------------------------------------------------------------------------
-setopt COMBINING_CHARS      # Combine zero-length punctuation characters (accents) with the base character.
-setopt INTERACTIVE_COMMENTS # allow the use of `# ` in the command line
-setopt NO_MAIL_WARNING      # Don't print a warning message if a mail file has been accessed.
-setopt RC_QUOTES            # Allow 'Henry''s Garage' instead of 'Henry'\''s Garage'.
-
-# IO
+# Input/Output
 # -----------------------------------------------------------------------------
 setopt IGNORE_EOF           # Disable '^D' logout keybind
+setopt INTERACTIVE_COMMENTS # allow the use of `# ` in the command line
 setopt NO_CLOBBER           # Disables overwrite existing files with `>`. Use `> | ` or `>!` instead
 setopt NO_CORRECT           # Disable zsh correct
+setopt NO_FLOW_CONTROL      # Disable start/stop characters in shell editor.
+setopt NO_MAIL_WARNING      # Don't print a warning message if a mail file has been accessed.
+setopt PATH_DIRS            # Perform path search even on command names with slashes.
+setopt RC_QUOTES            # Allow 'Henry''s Garage' instead of 'Henry'\''s Garage'.
 
 # Jobs
 # -----------------------------------------------------------------------------
-setopt LONG_LIST_JOBS       # List jobs in the long format by default.
 setopt AUTO_RESUME          # Attempt to resume existing job before creating a new process.
+setopt LONG_LIST_JOBS       # List jobs in the long format by default.
 setopt NOTIFY               # Report status of background jobs immediately.
 setopt NO_BG_NICE           # Don't run all background jobs at a lower priority.
-setopt NO_HUP               # Don't kill jobs on shell exit.
 setopt NO_CHECK_JOBS        # Don't report on jobs when shell exit.
+setopt NO_HUP               # Don't kill jobs on shell exit.
 
-# Globbing
+# Expansion and Globbing
 # -----------------------------------------------------------------------------
 setopt BRACE_CCL            # Allow brace character class list expansion.
 setopt EXTENDED_GLOB        # Use extended globbing syntax.
 setopt GLOB_DOTS            # Do not require a leading `.' in a filename to be matched explicitly.
 setopt MULTIOS              # Write to multiple descriptors.
+setopt NO_CASE_GLOB         # Make globbing (filename generation) sensitive to case.
 
 # Navigation
 # -----------------------------------------------------------------------------
@@ -39,6 +38,10 @@ setopt PUSHD_IGNORE_DUPS    # Do not store duplicates in the stack.
 setopt PUSHD_MINUS          # Replace 'cd -' with 'cd +'
 setopt PUSHD_SILENT         # Do not print the directory stack after pushd or popd.
 setopt PUSHD_TO_HOME        # Push to home directory when no argument is given.
+
+# ZLE
+# -----------------------------------------------------------------------------
+setopt COMBINING_CHARS      # Combine zero-length punctuation characters (accents) with the base character.
 
 # Less
 # ------------------------------------------------------------------------------
@@ -55,12 +58,6 @@ export LESSKEY=${XDG_CONFIG_HOME}/less/less
 
 if (( $#commands[(i)lesspipe(|.sh)] )); then
   export LESSOPEN="| /usr/bin/env $commands[(i)lesspipe(|.sh)] %s 2>&-"
-fi
-
-if [[ -d /usr/local/share/zsh/help ]]; then
-  unalias run-help
-  autoload run-help
-  HELPDIR=/usr/local/share/zsh/help
 fi
 
 # Editor
