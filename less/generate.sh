936940b2a2c0b59c01ed50868308df50b3a02fed
Adam Reese <adam@reese.io>
2021-05-07 11:31:58 -0700

    [less] create config directory if doesn't exist

--- less/generate.sh
+++ less/generate.sh
@@ -2,6 +2,11 @@
 set -euo pipefail
 
 DOTFILES=${DOTFILES:-${HOME}/.dotfiles}
-[[ -e "${DOTFILES}" ]] || { echo "${DOTFILES} directory does not exist"; exit 1; }
+[[ -e "${DOTFILES}" ]] || { echo "${DOTFILES} directory does not exist" 1>&2; exit 1; }
+
+export XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
+export LESSKEY=${LESSKEY:-${XDG_CONFIG_HOME}/less/less}
+
+mkdir -p "${XDG_CONFIG_HOME}/less"
 
 lesskey "${DOTFILES}/less/lesskey"
