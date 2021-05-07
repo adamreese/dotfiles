03d7a6fa0ee99fb97d1913dcc96855f24438ad1b
Adam Reese <adam@reese.io>
2021-05-07 10:45:49 -0700

    [lua] add install script for lua language server

---
+++ lua/install.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+((TRACE)) && set -x
+
+set -euo pipefail
+
+build_path="${XDG_DATA_HOME}/lsp/sumneko_lua"
+
+build_server() {
+  echo ':: Building sumneko lua-language-server'
+  cd "${build_path}/3rd/luamake"
+  ninja -f compile/ninja/macos.ninja
+
+  cd "${build_path}"
+  ./3rd/luamake/luamake rebuild
+}
+
+if [[ ! -d "${build_path}" ]]; then
+  mkdir -p "${build_path}"
+  git clone https://github.com/sumneko/lua-language-server "${build_path}"
+
+  cd "${build_path}"
+
+  git submodule update --init --recursive
+  build_server
+fi
+
+cd "${build_path}"
+
+git -c gc.auto=0 fetch --quiet --no-tags --recurse-submodules=no >/dev/null 2>&1
+
+behind=$(git rev-list --right-only --count HEAD...@'{u}' 2>/dev/null)
+if (( behind )); then
+  git pull --quiet
+  git submodule update --init --recursive
+
+  git --no-pager log --no-merges --stat --color ORIG_HEAD..
+
+  build_server
+fi
