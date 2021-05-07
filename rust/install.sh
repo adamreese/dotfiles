36d43b644b1c5dc3d95292345d6a6af3416b08d6
Adam Reese <adam@reese.io>
2021-05-07 10:45:07 -0700

    [rust] add install script for rust tools

---
+++ rust/install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+DOTFILES=${DOTFILES:-${HOME}/.dotfiles}
+[[ -e "${DOTFILES}" ]] || { echo "${DOTFILES} directory does not exist"; exit 1; }
+
+rustup completions zsh       >"${DOTFILES}/zsh/completion/_rustup"
+rustup completions zsh cargo >"${DOTFILES}/zsh/completion/_cargo"
+
+# for checking and applying updates to installed executables
+cargo install cargo-update
+cargo install cargo-edit cargo-play cargo-expand
