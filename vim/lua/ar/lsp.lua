ae318c354aaf2cf54da67ff5784c83f787c9e184
Adam Reese <adam@reese.io>
2021-09-21 10:19:54 -0700

    [vim] add lsp commands for built in functions

--- vim/lua/ar/lsp.lua
+++ vim/lua/ar/lsp.lua
@@ -70,13 +70,28 @@ local on_attach = function(client, bufnr)
 
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
+  vim.cmd [[command! LspDefinition lua vim.lsp.buf.definition()]]
+  vim.cmd [[command! LspDeclaration lua vim.lsp.buf.definition()]]
+  vim.cmd [[command! LspCodeAction lua vim.lsp.buf.code_action()]]
+  vim.cmd [[command! LspHover lua vim.lsp.buf.hover()]]
+  vim.cmd [[command! LspRename lua vim.lsp.buf.rename()]]
+  vim.cmd [[command! LspReferences lua vim.lsp.buf.references()]]
+  vim.cmd [[command! LspTypeDefinition lua vim.lsp.buf.type_definition()]]
+  vim.cmd [[command! LspImplementation lua vim.lsp.buf.implementation()]]
+  vim.cmd [[command! LspDiagPrev lua vim.lsp.diagnostic.goto_prev()]]
+  vim.cmd [[command! LspDiagNext lua vim.lsp.diagnostic.goto_next()]]
+  vim.cmd [[command! LspDiagLine lua vim.lsp.diagnostic.show_line_diagnostics()]]
+  vim.cmd [[command! LspSignatureHelp lua vim.lsp.buf.signature_help()]]
+  vim.cmd [[command! LspDocumentSymbol lua vim.lsp.document_symbol()]]
+  vim.cmd [[command! LspWorkspaceSymbol lua vim.lsp.buf.workspace_symbol()]]
+
   -- Mappings.
   map('n', 'gD',         '<Cmd>lua vim.lsp.buf.declaration()<CR>')
   map('n', 'gd',         '<Cmd>lua vim.lsp.buf.definition()<CR>')
   map('n', 'gs',         '<Cmd>split | lua vim.lsp.buf.definition()<CR>')
   map('n', '<leader>gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
   map('n', '<leader>k',  '<cmd>lua vim.lsp.buf.signature_help()<CR>')
-  map('n', 'gdt',        '<cmd>lua vim.lsp.buf.type_definition()<CR>')
+  map('n', '<leader>gd', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
   map('n', '<leader>gr', '<cmd>lua vim.lsp.buf.references()<CR>')
   map('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>')
 
