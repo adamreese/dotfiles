535afb0d0e31be02ac8de38b53863fb4c5e2cc4e
Adam Reese <adam@reese.io>
2021-09-30 09:48:29 -0700

    [python] fix pip install command

--- python/install.sh
+++ python/install.sh
@@ -5,7 +5,7 @@ DOTFILES=${DOTFILES:-${HOME}/.dotfiles}
 [[ -e "${DOTFILES}" ]] || { echo "${DOTFILES} directory does not exist"; exit 1; }
 
 echo "Updating global pip"
-pip3 install --upgrade pip
+python3 -m pip install --upgrade pip
 
 echo "Updating global pip requirements"
-pip3 install --upgrade --requirement "${DOTFILES}/python/requirements.txt"
+python3 -m pip install --upgrade --requirement "${DOTFILES}/python/requirements.txt"
