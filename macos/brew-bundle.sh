24ea92ab554538fa23b841a6c7b4247725dcf270
Adam Reese <adam@reese.io>
2021-10-06 10:07:21 -0700

    [macos] brew-bundle.sh is not needed
    This was a helper for setting the Brewfile.  Brew bundle works When
    running commands in the directory with the Brewfile

--- macos/brew-bundle.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euxo pipefail
-
-DOTFILES=${DOTFILES:-${HOME}/.dotfiles}
-[[ -e "${DOTFILES}" ]] || die "${DOTFILES} directory does not exist"
-
-brewfile="${DOTFILES}/macos/Brewfile"
-
-brew bundle dump --describe --force --file="${brewfile}"
