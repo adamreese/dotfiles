cddbd3d816b27993f903fad3043ad3b213da47b8
Adam Reese <adam@reese.io>
2021-11-27 08:32:28 -0800

    [zsh] remove fzf completion
    I never use it

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -12,39 +12,10 @@ function {
   local _opts=(
     --color 'fg:15,bg:-1,bg+:-1,pointer:1,info:7,hl+:4,hl:4'
     --multi
-    --bind 'ctrl-u:page-up,ctrl-d:page-down'
+    --bind 'alt-p:preview-up,alt-n:preview-down'
+    --bind 'alt-k:preview-up,alt-j:preview-down'
     --bind '?:toggle-preview'
     --preview-window 'right:62%:hidden'
   )
   typeset -gx FZF_DEFAULT_OPTS="${(f)_opts}"
 }
-
-# Use ',' as the trigger sequence instead of the default **
-export FZF_COMPLETION_TRIGGER=','
-
-# ------------------------------------------------------------------------------
-
-# Use fd (https://github.com/sharkdp/fd) instead of the default find
-# command for listing path candidates.
-# - The first argument to the function ($1) is the base path to start traversal
-# - See the source code (completion.{bash,zsh}) for the details.
-_fzf_compgen_path() {
-  fd --hidden --follow --exclude ".git" . "$1"
-}
-
-# Use fd to generate the list for directory completion
-_fzf_compgen_dir() {
-  fd --type d --hidden --follow --exclude ".git" . "$1"
-}
-
-if [[ -s "/usr/local/opt/fzf/shell/completion.zsh" ]]; then
-  source "/usr/local/opt/fzf/shell/completion.zsh"
-fi
-
-zle -N fzf-completion
-bindkey -M viins '^I' fzf-completion
-
-# [Ctrl-X /] fzf history
-zle -N fzf-history-widget
-bindkey -M viins '^X/' fzf-history-widget
-bindkey -M emacs '^X/' fzf-history-widget
