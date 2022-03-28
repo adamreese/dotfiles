c39b95c6730b42113e68556dc29df92690072345
Adam Reese <adam@reese.io>
2022-03-28 11:01:02 -0700

    [vim] add gitsigns mappings

--- vim/plugin/plug/gitsigns.lua
+++ vim/plugin/plug/gitsigns.lua
@@ -1,17 +1,28 @@
-local has_gitsigns, gitsigns = pcall(require, 'gitsigns')
+local has_gitsigns, gitsigns = pcall(require, "gitsigns")
 
 if not has_gitsigns then
-  print('[WARN] gitsigns not found/installed/loaded..')
+  vim.notify("gitsigns not found/installed/loaded..", vim.log.levels.WARN)
   return
 end
 
-gitsigns.setup({
-  debug_mode = true,
-  signs = {
-    add = { hl = 'GitGutterAdd', text = '│' },
-    change = { hl = 'GitGutterChange', text = '│' },
-    delete = { hl = 'GitGutterDelete', text = '_' },
-    topdelete = { hl = 'GitGutterDelete', text = '‾' },
-    changedelete = { hl = 'GitGutterChangeDelete', text = '~' },
-  },
-})
+gitsigns.setup(
+  {
+    debug_mode = true,
+    signs = {
+      add = {hl = "GitGutterAdd", text = "│"},
+      change = {hl = "GitGutterChange", text = "│"},
+      delete = {hl = "GitGutterDelete", text = "_"},
+      topdelete = {hl = "GitGutterDelete", text = "‾"},
+      changedelete = {hl = "GitGutterChangeDelete", text = "~"}
+    },
+    keymaps = {
+      noremap = true,
+      buffer = true,
+      ["n ]c"] = {expr = true, [[&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>']]},
+      ["n [c"] = {expr = true, [[&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>']]},
+      -- Text objects
+      ['o ih'] = ':<C-U>Gitsigns select_hunk()<CR>',
+      ['x ih'] = ':<C-U>Gitsigns select_hunk()<CR>',
+    }
+  }
+)
