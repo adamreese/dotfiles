a5899a41ac9885a10183e0a853c103932c3e368b
Adam Reese <adam@reese.io>
2022-05-10 18:05:37 -0700

    [rust] code formatting

--- rust/install.sh
+++ rust/install.sh
@@ -11,5 +11,4 @@ rustup completions zsh >"${DOTFILES}/zsh/completion/_rustup"
 rustup completions zsh cargo >"${DOTFILES}/zsh/completion/_cargo"
 
 # for checking and applying updates to installed executables
-cargo install cargo-update
-cargo install cargo-edit cargo-play cargo-expand
+cargo install cargo-update cargo-edit cargo-play cargo-expand
