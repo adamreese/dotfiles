2803150a153af2c249eb5d8a9df62d19d0b706e2
Adam Reese <adam@reese.io>
2022-10-27 09:07:12 -0700

    [vim] smarter gitsigns mapping

--- vim/lua/ar/gitsigns.lua
+++ vim/lua/ar/gitsigns.lua
@@ -18,11 +18,21 @@ gitsigns.setup(
     keymaps = {
       noremap = true,
       buffer = true,
-      ['n ]c'] = { expr = true, [[&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>']] },
-      ['n [c'] = { expr = true, [[&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>']] },
-      -- Text objects
       ['o ih'] = ':<C-U>Gitsigns select_hunk()<CR>',
       ['x ih'] = ':<C-U>Gitsigns select_hunk()<CR>',
-    }
+    },
+    on_attach = function()
+      vim.keymap.set('n', ']c', function()
+        if vim.wo.diff then return ']c' end
+        vim.schedule(function() gitsigns.next_hunk() end)
+        return '<Ignore>'
+      end, { expr = true, desc = 'jump to next hunk' })
+
+      vim.keymap.set('n', '[c', function()
+        if vim.wo.diff then return ']c' end
+        vim.schedule(function() gitsigns.prev_hunk() end)
+        return '<Ignore>'
+      end, { expr = true, desc = 'jump to prev hunk ' })
+    end,
   }
 )
