a42034407452dddbbafb51d11e195ec874a27442
Adam Reese <adam@reese.io>
2022-10-27 08:23:03 -0700

    [vim] cmp_nvim_lsp.update_capabilities is deprecated

--- vim/lua/ar/lsp/init.lua
+++ vim/lua/ar/lsp/init.lua
@@ -233,7 +233,7 @@ local function setup_servers()
   status.register_progress()
 
   local capabilities = vim.lsp.protocol.make_client_capabilities()
-  capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+  capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
   capabilities = vim.tbl_extend('keep', capabilities, status.capabilities)
 
   local function with_defaults(opts)
