3abd3d88854cc2afc119f445f664acd3e77e0319
Adam Reese <adam@reese.io>
2023-02-13 15:26:43 -0800

    [vim] remove lsp diagnostics icons

--- vim/lua/ar/lsp/init.lua
+++ vim/lua/ar/lsp/init.lua
@@ -39,10 +39,10 @@ vim.lsp.protocol.CompletionItemKind = {
   '♛ [type]',
 }
 
-vim.fn.sign_define('DiagnosticSignError', { text = '❯', texthl = 'DiagnosticSignError', numhl = 'DiagnosticSignError' })
-vim.fn.sign_define('DiagnosticSignHint', { text = '❯', texthl = 'DiagnosticSignHint', numhl = 'DiagnosticSignHint' })
-vim.fn.sign_define('DiagnosticSignInfo', { text = '❯', texthl = 'DiagnosticSignInfo', numhl = 'DiagnosticSignInfo' })
-vim.fn.sign_define('DiagnosticSignWarn', { text = '❯', texthl = 'DiagnosticSignWarn', numhl = 'DiagnosticSignWarn' })
+vim.fn.sign_define('DiagnosticSignError', { text = '', texthl = 'DiagnosticSignError', numhl = 'DiagnosticSignError' })
+vim.fn.sign_define('DiagnosticSignHint', { text = '', texthl = 'DiagnosticSignHint', numhl = 'DiagnosticSignHint' })
+vim.fn.sign_define('DiagnosticSignInfo', { text = '', texthl = 'DiagnosticSignInfo', numhl = 'DiagnosticSignInfo' })
+vim.fn.sign_define('DiagnosticSignWarn', { text = '', texthl = 'DiagnosticSignWarn', numhl = 'DiagnosticSignWarn' })
 
 vim.diagnostic.config({
   underline = true,
