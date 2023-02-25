3f42c2c626bcea6c281b21a4ed57ac1d88013ba7
Adam Reese <adam@reese.io>
2023-02-25 12:56:25 -0800

    [vim] use default signs for gitsigns

--- vim/lua/ar/gitsigns.lua
+++ vim/lua/ar/gitsigns.lua
@@ -8,13 +8,6 @@ end
 gitsigns.setup(
   {
     debug_mode = true,
-    signs = {
-      add = { hl = 'GitGutterAdd', text = '│' },
-      change = { hl = 'GitGutterChange', text = '│' },
-      delete = { hl = 'GitGutterDelete', text = '_' },
-      topdelete = { hl = 'GitGutterDelete', text = '‾' },
-      changedelete = { hl = 'GitGutterChangeDelete', text = '~' }
-    },
     keymaps = {
       noremap = true,
       buffer = true,
