24b1736d0c4d26b9c6e79efc436b71ff50e7c406
Adam Reese <adam@reese.io>
2023-03-16 18:22:05 -0700

    [rust] remove cargo completion setup
    Cargo completion is included with brew

--- rust/install.sh
+++ rust/install.sh
@@ -8,7 +8,6 @@ DOTFILES=${DOTFILES:-${HOME}/.dotfiles}
 }
 
 rustup completions zsh >"${DOTFILES}/zsh/completion/_rustup"
-rustup completions zsh cargo >"${DOTFILES}/zsh/completion/_cargo"
 
 # for checking and applying updates to installed executables
 cargo install cargo-update cargo-edit cargo-play cargo-expand
