7fa9ad42fe3ccd0ae778b47d2c5da5491959abad
Adam Reese <adam@reese.io>
2023-04-10 11:46:46 -0700

    [vim] add mapping to display syntax highlighting

--- vim/lua/ar/plugins/treesitter.lua
+++ vim/lua/ar/plugins/treesitter.lua
@@ -4,7 +4,10 @@ return {
   build = ':TSUpdate',
   dependencies = {
     'nvim-treesitter/nvim-treesitter-textobjects',
-    'nvim-treesitter/playground',
+    {
+      'nvim-treesitter/playground',
+      keys = { { '<leader>H', '<cmd>TSHighlightCapturesUnderCursor<cr>' } },
+    },
   },
   config = function()
     require('nvim-treesitter.configs').setup({
