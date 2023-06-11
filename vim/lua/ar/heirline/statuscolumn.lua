d00d496b11401d87cde6bce6b9068926138751bc
Adam Reese <adam@reese.io>
2023-06-10 19:50:11 -0700

    [vim] add diagnostic signs to statuscolumn

--- vim/lua/ar/heirline/statuscolumn.lua
+++ vim/lua/ar/heirline/statuscolumn.lua
@@ -1,3 +1,50 @@
+local diagnostics = {
+  static = {
+    texts = {
+      DiagnosticSignError = ' ',
+      DiagnosticSignWarn  = '⚠ ',
+      DiagnosticSignInfo  = ' ',
+      DiagnosticSignHint  = ' ',
+    },
+  },
+  init = function(self)
+    local signs = vim.fn.sign_getplaced(vim.api.nvim_get_current_buf(), {
+      group = '*',
+      lnum = vim.v.lnum,
+    })
+
+    if #signs == 0 or signs[1].signs == nil then
+      self.sign = nil
+      self.has_sign = false
+      return
+    end
+
+    signs = vim.tbl_filter(function(sign)
+      return vim.startswith(sign.group, 'vim.diagnostic')
+    end, signs[1].signs)
+
+    if #signs == 0 then
+      self.sign = nil
+    else
+      self.sign = signs[1]
+    end
+
+    self.has_sign = self.sign ~= nil
+  end,
+  provider = function(self)
+    if self.has_sign then
+      return self.texts[self.sign.name]
+    end
+
+    return '  '
+  end,
+  hl = function(self)
+    if self.has_sign then
+      return self.sign.name
+    end
+  end,
+}
+
 function fix_gitsign_name(name)
   local names = {
     GitSignsAddAdd = 'GitSignsAdd',
@@ -45,6 +92,7 @@ local line_numbers = {
 }
 
 return {
+  diagnostics,
   { provider = '%=' },
   line_numbers,
   gitsigns,
