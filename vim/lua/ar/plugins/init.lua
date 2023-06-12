bf0a0d59bbebd1c804a2bf284461b944613a2d47
Adam Reese <adam@reese.io>
2023-06-12 15:59:36 -0700

    [vim] change trouble mapping

--- vim/lua/ar/plugins/init.lua
+++ vim/lua/ar/plugins/init.lua
@@ -178,7 +178,7 @@ return {
     },
     keys = {
       { '<leader>xx', '<cmd>TroubleToggle document_diagnostics<cr>',  desc = 'Document Diagnostics (Trouble)' },
-      { '<leader>xX', '<cmd>TroubleToggle workspace_diagnostics<cr>', desc = 'Workspace Diagnostics (Trouble)' },
+      { '<leader>xw', '<cmd>TroubleToggle workspace_diagnostics<cr>', desc = 'Workspace Diagnostics (Trouble)' },
       { '<leader>xl', '<cmd>TroubleToggle loclist<cr>',               desc = 'Location List (Trouble)' },
       { '<leader>xq', '<cmd>TroubleToggle quickfix<cr>',              desc = 'Quickfix List (Trouble)' },
     },
