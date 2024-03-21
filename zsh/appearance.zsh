db4aa891996c719cfa500acaa09192f522f03ee4
Adam Reese <adam@reese.io>
2024-03-20 19:49:28 -0700

    [zsh] add hyperlinks for files using ls

--- zsh/appearance.zsh
+++ zsh/appearance.zsh
@@ -11,11 +11,11 @@ else
   export LS_COLORS='di=1;34:ln=35:so=32:pi=33:ex=31:bd=1;36:cd=1;33:su=30;41:sg=30;46:tw=30;42:ow=30;43'
 fi
 
-(( $+commands[gls] )) || alias gls='ls'
+(( ! $+commands[gls] )) || alias gls='ls'
 
 if gls --color > /dev/null 2>&1; then colorflag='--color'; else colorflag='-G'; fi;
 
-alias ls="gls -Ah ${colorflag} --group-directories-first"
+alias ls="gls -Ah ${colorflag} --group-directories-first --hyperlink=auto"
 alias l='ls -l'
 
 # Take advantage of $LS_COLORS for completion as well.
