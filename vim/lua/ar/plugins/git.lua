8c3d4507462c63473758d9c7b4fbdee1cc3fb97d
Adam Reese <adam@reese.io>
2024-04-16 17:49:07 -0700

    [vim] add gitsigns icons

--- vim/lua/ar/plugins/git.lua
+++ vim/lua/ar/plugins/git.lua
@@ -4,6 +4,14 @@ return {
     event = { 'BufReadPre', 'BufNewFile' },
     dependencies = { 'nvim-lua/plenary.nvim' },
     opts = {
+      signs = {
+        add          = { text = ' ▏' },
+        change       = { text = ' ▏' },
+        delete       = { text = ' _' },
+        topdelete    = { text = ' ‾' },
+        changedelete = { text = ' ~' },
+        untracked    = { text = ' ▏' },
+      },
       debug_mode = true,
       on_attach = function()
         vim.keymap.set('n', ']c', function()
