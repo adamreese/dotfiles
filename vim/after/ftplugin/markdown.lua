300f0c7d8a557a013990f4d6805c4767bf28ca72
Adam Reese <adam@reese.io>
2024-06-29 15:35:46 -0700

    [vim] move markdown ftplugin to after/ftplugin

---
+++ vim/after/ftplugin/markdown.lua
@@ -0,0 +1,15 @@
+vim.opt_local.shiftwidth = 2
+vim.opt_local.tabstop = 2
+vim.opt_local.softtabstop = 2
+vim.opt_local.spell = true
+vim.opt_local.textwidth = 80
+vim.opt_local.foldenable = false
+vim.opt_local.conceallevel = 0
+
+vim.g.vim_markdown_fenced_languages = {
+  'bash=sh',
+  'erb=eruby',
+  'js=javascript',
+  'less=css',
+  'zsh=sh',
+}
