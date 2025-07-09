a9a128f9fa191f64f883e746c89e1ab5e2388380
Adam Reese <adam@reese.io>
2025-07-08 20:32:42 -0700

    [vim] configure diagnostic signs in vim.diagnostic.config

--- vim/plugin/diagnostics.lua
+++ vim/plugin/diagnostics.lua
@@ -1,7 +1,6 @@
 vim.diagnostic.config({
   underline = true,
   virtual_text = false,
-  signs = true,
   update_in_insert = false,
   severity_sort = true,
   jump = { float = true },
@@ -12,9 +11,17 @@ vim.diagnostic.config({
     source = true,
     header = '',
   },
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = '',
+      [vim.diagnostic.severity.WARN] = '󰔷',
+      [vim.diagnostic.severity.HINT] = '',
+      [vim.diagnostic.severity.INFO] = '',
+    },
+  },
 })
 
-vim.fn.sign_define('DiagnosticSignError', { text = '', texthl = 'DiagnosticSignError' })
-vim.fn.sign_define('DiagnosticSignWarn', { text = '󰔷', texthl = 'DiagnosticSignWarn' })
-vim.fn.sign_define('DiagnosticSignInfo', { text = '', texthl = 'DiagnosticSignInfo' })
-vim.fn.sign_define('DiagnosticSignHint', { text = '', texthl = 'DiagnosticSignHint' })
+-- vim.fn.sign_define('DiagnosticSignError', { text = '', texthl = 'DiagnosticSignError' })
+-- vim.fn.sign_define('DiagnosticSignWarn', { text = '󰔷', texthl = 'DiagnosticSignWarn' })
+-- vim.fn.sign_define('DiagnosticSignInfo', { text = '', texthl = 'DiagnosticSignInfo' })
+-- vim.fn.sign_define('DiagnosticSignHint', { text = '', texthl = 'DiagnosticSignHint' })
