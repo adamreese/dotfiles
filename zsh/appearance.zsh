2d2ddee75ab11b10b7c4be1e4f254da6c36d1031
Adam Reese <adam@reese.io>
2026-02-27 15:29:15 -0800

    [zsh] update location of dircolors config

--- zsh/appearance.zsh
+++ zsh/appearance.zsh
@@ -1,11 +1,11 @@
 # ls colors
 # ------------------------------------------------------------------------------
 
-if (( ! ${+LS_COLORS} )) && [[ -s ${XDG_CONFIG_HOME}/dircolors ]]; then
+if (( ! ${+LS_COLORS} )) && [[ -s ${DOTFILES}/dircolors ]]; then
   if (( $+commands[dircolors] )); then
-    eval "$(dircolors --sh ${XDG_CONFIG_HOME}/dircolors)"
+    eval "$(dircolors --sh ${DOTFILES}/dircolors)"
   elif (( $+commands[gdircolors] )); then
-    eval "$(gdircolors --sh ${XDG_CONFIG_HOME}/dircolors)"
+    eval "$(gdircolors --sh ${DOTFILES}/dircolors)"
   fi
 else
   export LS_COLORS='di=1;34:ln=35:so=32:pi=33:ex=31:bd=1;36:cd=1;33:su=30;41:sg=30;46:tw=30;42:ow=30;43'
