2c13010a269477243741097e351380d11d72051a
Adam Reese <adam@reese.io>
2026-02-27 15:31:04 -0800

    [zsh] export termcap vars

--- zsh/environment.zsh
+++ zsh/environment.zsh
@@ -55,6 +55,14 @@ export LESSHISTFILE=${XDG_STATE_HOME}/less_history
 
 export PAGER=less
 
+export LESS_TERMCAP_mb=$'\E[1;34m'
+export LESS_TERMCAP_md=$'\E[1;31m'
+export LESS_TERMCAP_me=$'\E[0m'
+export LESS_TERMCAP_se=$'\E[0m'
+export LESS_TERMCAP_so=$'\E[1;1;40m'
+export LESS_TERMCAP_ue=$'\E[0m'
+export LESS_TERMCAP_us=$'\E[1;32m'
+
 # Editor
 # -----------------------------------------------------------------------------
 export EDITOR=vim
